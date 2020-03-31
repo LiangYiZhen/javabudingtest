2eaf22a742324e214b9efdcc1b552d079e38f906
liangyizhen <327935486@qq.com>
2020-03-31 16:28:36 +0800

    Initial commit

---
+++ src/com/test/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test.java;
+
+public class HelloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("helloworld!!!");
+
+                
+    }
+
+}
